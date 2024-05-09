54c154b8cbdcfa960d8fa30e5327bcda0d8f188a
krys11 <krysdario1996@gmail.com>
2024-05-09 16:32:38 -0400

    all

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5MKcN_Z8oP2Q9pXrgtDeo_FlGuBxbNkU",
@@ -8,11 +9,15 @@ const firebaseConfig = {
   storageBucket: "tourismbenin-5537d.appspot.com",
   messagingSenderId: "436146337211",
   appId: "1:436146337211:web:a6d822b7a7b6a50886409e",
-  measurementId: "G-K3BLDD4DM2"
+  measurementId: "G-K3BLDD4DM2",
 };
 
+const app = initializeApp(firebaseConfig);
+
 // Initialise Firebase
-firebase.initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 // Exportez le service authentification
-export const auth = firebase.auth();
+export { auth };
